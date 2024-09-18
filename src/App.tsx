36d8fb5434678bb490dbc0e0fb88b81d24b7bf91
YUKI <yuki.marchal@edu.technobel.be>
2024-09-18 16:43:12 +0200

    Added comments and README

--- src/App.tsx
+++ src/App.tsx
@@ -18,27 +18,45 @@ interface Address {
 
 function App() {
 
+  // useRef pour stocker les données du champ de saisie
   const idRef = useRef<HTMLInputElement>(null);
+
+  // useState pour stocker des données qui peuvent changer avec le temps
+  // Lorsqu'il y a un changement, cela déclenche un re-render
   const [selectedId, setSelectedId] = useState<number>();
   const [users, setUsers] = useState<User[]>([])
 
+  // useEffect pour exécuter des actions asynchrones qui peuvent affecter d'autres composants
   useEffect(() => {
+    // axios est une bibliothèque JS populaire pour effectuer des requêtes HTTP
     axios
+    // "get" une promesse à partir de l'URL et "then" exécuter une action avec la "réponse (= promesse)"
       .get<User[]>('https://jsonplaceholder.typicode.com/users')
       .then((response) => {
+        // Tant que la référence est la même, setState considère qu'il n'y a pas de changement. Pour indiquer le changement dans un tableau ou un objet, nous devons créer un nouveau tableau et copier ce qu'il y avait à l'intérieur en faisant [...oldArray]
         setUsers([...response.data])
       })
   })
 
   const handleSubmit = (event: FormEvent) => {
+    // Par défaut, React rafraîchit la page lorsque le bouton de soumission est cliqué. Pour empêcher cela, nous utilisons preventDefault()
     event.preventDefault();
+    // A && B signifie que si A n'est pas null ou undefined, exécute B
+    // Ref.current.value est un string. Pour stocker dans une variable de type nombre, nous devons convertir en ajoutant "+" devant.
     idRef.current && setSelectedId(+idRef.current.value);
   }
 
   return (
     <div>
       <h1 className='text-center m-3'>User</h1>
+      {/* 
+      // onSubmit définit une action à exécuter lorsque le bouton est cliqué.
+      // Attention, il n'y a pas de () après le nom de la méthode
+      */}
       <form className='mb-3' onSubmit={handleSubmit}>
+        {/* 
+        // ref obtient la valeur d'entrée et l'affecte au Ref hook indiqué
+        */}
         <input className='form-control' type="number" ref={idRef} />
         <button className="btn btn-primary my-3" type="submit">
           Search
@@ -46,7 +64,11 @@ function App() {
       </form>
       {idRef && <p className='fw-bold'>Selected ID : {selectedId}</p>}
       <div className='grid-container'>
+      {/* 
+      // A && B signifie que si A n'est pas null ou undefined, exécute B
+      */}
         {selectedId && users &&
+        // map() est comme un forEach. (pour chaque utilisateur) => {exécute une action}
           users.map((user) => user.id === selectedId &&
             <div key={user.id}>
               <p>ID: {user.id}</p>
---
+++ src/README.md
@@ -0,0 +1,65 @@
+**Créer une application React**
+
+
+*1. Vérifiez la version de Node dans le terminal*
+
+	node -v
+
+
+*2. Si Node est antérieur à la version 16, installez une version plus récente*
+
+
+*3. Dans le terminal, suivez ces étapes =>*
+
+
+	npm create vite@latest
+
+	NomDuProjet
+
+	React
+
+	TypeScript
+
+	npm run dev
+
+
+*4. Voici votre première application React*
+
+
+*Lorsque vous clonez une application React, n'oubliez pas de faire ceci =>*
+
+	npm i
+
+
+
+**Utiliser Bootstrap**
+
+
+*1. Installez Bootstrap dans le terminal*
+
+	npm i bootstrap@latest
+
+
+*2. Ajoutez l'importation dans main.tsx*
+
+	import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+
+*3. Trouvez une classe liée au CSS que vous souhaitez sur =>*
+
+	https://getbootstrap.com/docs/5.3/getting-started/introduction/
+
+
+*4. Ajoutez la classe à votre élément HTML*
+
+
+
+**Utiliser Axios**
+
+
+*1. Installez Axios dans le terminal*
+
+	npm i axios@latest
+
+*2. Ajoutez l'importation dans le composant où vous souhaitez utiliser Axios*
+
