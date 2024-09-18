6bda31826b6136f7fbd0ab7eaddbc6b102656034
YUKI <yuki.marchal@edu.technobel.be>
2024-09-18 15:51:38 +0200

    Fetched data from API and rendered on the page => Effect hook, map( )

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import { FormEvent, useRef, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import './App.css'
+import axios from 'axios';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  address: Address;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+}
 
 function App() {
 
-  const [selectedId, setSelectedId] = useState('');
   const idRef = useRef<HTMLInputElement>(null);
+  const [selectedId, setSelectedId] = useState<number>();
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setUsers([...response.data])
+      })
+  })
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-
-    idRef.current && setSelectedId(idRef.current.value);
+    idRef.current && setSelectedId(+idRef.current.value);
   }
 
   return (
     <div>
+      <h1 className='text-center m-3'>User</h1>
       <form className='mb-3' onSubmit={handleSubmit}>
-      <input className='form-control' type="number" ref={idRef}/>
-      <button className="btn btn-primary my-3" type="submit">
-            Search
-          </button>
-          </form>
-      {idRef && <p>Selected ID : {selectedId}</p>}
+        <input className='form-control' type="number" ref={idRef} />
+        <button className="btn btn-primary my-3" type="submit">
+          Search
+        </button>
+      </form>
+      {idRef && <p className='fw-bold'>Selected ID : {selectedId}</p>}
+      <div className='grid-container'>
+        {selectedId && users &&
+          users.map((user) => user.id === selectedId &&
+            <div key={user.id}>
+              <p>ID: {user.id}</p>
+              <p>NAME: {user.name}</p>
+              <p>EMAIL: {user.email}</p>
+              <p>ADDRESS: {user.address.street}, {user.address.suite}, {user.address.zipcode}, {user.address.city}</p>
+            </div>
+          )}
+      </div>
     </div>
   )
 }
