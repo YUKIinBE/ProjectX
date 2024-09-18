ab5e43fcddb111435d242447952ad2c68d5cd05c
YUKI <yuki.marchal@edu.technobel.be>
2024-09-18 15:17:38 +0200

    Created search input form => State hook, Ref and event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { FormEvent, useRef, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [selectedId, setSelectedId] = useState('');
+  const idRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    idRef.current && setSelectedId(idRef.current.value);
+  }
+
   return (
     <div>
-      
+      <form className='mb-3' onSubmit={handleSubmit}>
+      <input className='form-control' type="number" ref={idRef}/>
+      <button className="btn btn-primary my-3" type="submit">
+            Search
+          </button>
+          </form>
+      {idRef && <p>Selected ID : {selectedId}</p>}
     </div>
   )
 }
